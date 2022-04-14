905eeefcf8d4c5c1dbcae0eff53ac96e040bec0c
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-04-14 14:08:20 +0530

    Check Given Number is Palindrome or Not

---
+++ Frequently Asked Java Programs/AT04_CheckPalindromeNumber.java
@@ -0,0 +1,32 @@
+//Check Given Number is Palindrome or Not
+
+import java.util.Scanner;
+
+public class AT04_CheckPalindromeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num = sc.nextInt();
+		sc.close();
+		
+		int org_num = num;
+		//reverse the number
+		int rev = 0;
+		
+		while(num != 0)
+		{
+			int rem = num % 10;
+			rev = rev * 10 + rem;
+			num = num / 10;		
+		}
+		
+		if(org_num == rev)
+			System.out.println(org_num + " is a palindrome number");
+		else
+			System.out.println(org_num + " is not a palindrome number");
+	}
+
+}
